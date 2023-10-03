5c5b2b81c5d14bb614bc70048e9002ab82022655
ouassilaaa <ouassidu31@hotmail.fr>
2023-10-03 20:57:27 +0200

    testing cypress

--- src/components/Inscription.js
+++ src/components/Inscription.js
@@ -3,9 +3,6 @@ import { useState } from 'react';
 import axios from 'axios';
 import { useNavigate } from "react-router-dom";
 import Card from '../UI/Card';
-// import {GrContact} from 'react-icons/gr'
-
-
 
 const Inscription = () => {
 
